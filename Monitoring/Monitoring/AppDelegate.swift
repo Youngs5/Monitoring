cfe5a189079da38e5d521f7546cad36b1f709915
Youngs5 <oys9330@gmail.com>
2023-11-02 14:05:53 +0900

    Feat: 앱 생성

---
+++ Monitoring/Monitoring/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Monitoring
+//
+//  Created by 오영석 on 11/2/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let contentViewController = MainViewController()
+        window = NSWindow(contentViewController: contentViewController)
+        window.title = "My macOS App"
+        window.setContentSize(NSSize(width: 800, height: 600))
+        window.center()
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+}
---
+++ Monitoring/Monitoring/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Monitoring
+//
+//  Created by 오영석 on 11/2/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+}
+
